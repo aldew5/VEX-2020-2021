ca55f4ddc17b24201ecd4b94f4f2e7e192846f30
Alec <alec@cisudbury.ca>
2020-11-05 19:38:09 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Alec Dewulf                                               */
+/*    Created:      Thu Nov 05 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include <vex_triport.h>
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+
+    
+}
